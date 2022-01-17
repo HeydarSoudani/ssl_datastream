252000f6ec5801f6d7c299df6d95f9b915e48ffb
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-17 15:38:12 +0330

    mlp

--- model.py
+++ model.py
@@ -34,8 +34,9 @@ class MyPretrainedResnet50(nn.Module):
     # freeze all layers but the last fc
     for name, param in self.pretrained.named_parameters():
       print(name)
-      print(name.startswith('layer4'))
-      if name not in ['fc.weight', 'fc.bias']:
+      print(not name.startswith(('layer4', 'fc')))
+      if not name.startswith(('layer4', 'fc')):
+      # if name not in ['fc.weight', 'fc.bias']:
         param.requires_grad = False
     self.pretrained.load_state_dict(state_dict, strict=False)
     
