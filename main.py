2988aba2857e937bbea7eddf6dfd25108a944d0d
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-17 12:05:19 +0330

    add trainer file

--- main.py
+++ main.py
@@ -55,30 +55,6 @@ parser.add_argument('--last_model_path', type=str, default='saved/model_last.pt'
 args = parser.parse_args()
 
 
-def test(model, test_loader, args, device):
-  model.to(device)
-
-  correct = 0
-  total = 0
-
-  model.eval()
-  with torch.no_grad():
-    for i, data in enumerate(test_loader):
-  
-      samples, labels = data
-      samples, labels = samples.to(device), labels.to(device)
-      logits, feature = model.forward(samples)
-      
-      _, predicted = torch.max(logits, 1)
-      total += labels.size(0)
-      correct += (predicted == labels).sum().item()
-    
-    print('Accuracy of the network on the 10000 test images: %7.4f %%' % (100 * correct / total))
-  return correct / total
-
-
-
-
 ## == Device ===========================
 if torch.cuda.is_available():
   if not args.cuda:
@@ -157,64 +133,9 @@ test_dataloader = DataLoader(dataset=test_dataset,
 
 
 # === train ===========================
-print('training ...')
-criterion = torch.nn.CrossEntropyLoss()
-optim = SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-scheduler = StepLR(optim, step_size=args.step_size, gamma=args.gamma)
-
-min_loss = float('inf')
-for epoch_item in range(args.start_epoch, args.epochs):
-  print('=== Epoch %d ===' % epoch_item)
-  train_loss = 0.
-  for i, batch in enumerate(train_dataloader):
-    images, labels = batch
-    images, labels = images.to(device), labels.to(device)
-    
-    optim.zero_grad()
-    outputs = model.forward(images)
-    loss = criterion(outputs, labels)
-    loss.backward()
-    optim.step()
-
-    train_loss += loss
-
-    # == Validation =================
-    if (i+1) % args.log_interval == 0:
-      with torch.no_grad():
-        total_val_loss = 0.0
-        model.eval()
-        for j, data in enumerate(val_dataloader):
-          sample, labels = data
-          sample, labels = sample.to(device), labels.to(device)
-
-          logits = model.forward(sample)
-          loss = criterion(logits, labels)
-          loss = loss.mean()
-          total_val_loss += loss.item()
-
-        total_val_loss /= len(val_dataloader)
-        print('=== Epoch: %d/%d, Train Loss: %f, Val Loss: %f' % (
-          epoch_item, i+1,  train_loss/args.log_interval, total_val_loss))
-        train_loss = 0.
-
-        # save best model
-        if total_val_loss < min_loss:
-          # model.save(os.path.join(args.save, "model_best.pt"))
-          torch.save(model.state_dict(), os.path.join(args.save, "model_best.pt"))
-          min_loss = total_val_loss
-          print("Saving new best model")
-  
-  if args.scheduler:
-    scheduler.step()
-  
-# save last model
-# model.save(os.path.join(args.save, "model_last.pt"))
-torch.save(model.state_dict(), os.path.join(args.save, "model_last.pt"))
-print("Saving new last model")
-
+train(model, train_dataloader, val_dataloader, args, device)
 
 ### === Test model =================
-print('Testing model ...')
 test(model, test_dataloader, args, device)
 
 
