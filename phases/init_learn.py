ac990363a5cfa81ed614a81e5155f64d2d6efbb1
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-19 23:30:56 +0330

    add relation

--- phases/init_learn.py
+++ phases/init_learn.py
@@ -28,6 +28,9 @@ def init_learn(feature_ext, relation, learner, args, device):
     # transforms.RandomErasing(probability=args.p, sh=args.sh, r1=args.r1, mean=[0.5, 0.5, 0.5]),
   ])
 
+  print(torch.tensor(train_data[-1]).shape)
+  print(torch.tensor(train_data[:-1]).shape)
+
   init_dataset = TensorDataset(
     torch.tensor(train_data[:-1]),
     torch.tensor(train_data[-1]).flatten()
