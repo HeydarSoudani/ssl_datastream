256da8ac07cfb065a9f0643cb684ae953335be64
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-19 12:22:06 +0330

    learner added

--- samplers/relation_sampler.py
+++ samplers/relation_sampler.py
@@ -36,7 +36,7 @@ class RelationSampler(Sampler):
   def __iter__(self):
     for _ in range(self.n_tasks):
       classes = random.sample(self.items_per_label.keys(), self.n_way)
-      query_class = random.sample(classes, 1)
+      query_class = random.sample(classes, 1)[0]
       support_class = classes.remove(query_class)
 
       print(classes)
