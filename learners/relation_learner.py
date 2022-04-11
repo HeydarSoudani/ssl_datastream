f6644ca11879e12cee0d297f45712f528c7b4324
Heydar <heydar@Heydars-MacBook-Pro.local>
2022-04-11 17:55:42 +0430

    print vars

--- learners/relation_learner.py
+++ learners/relation_learner.py
@@ -75,10 +75,10 @@ class RelationLearner:
     ### === Prepare PTs ================================
     n_known = len(known_labels)
     pt_per_class = 1
-    known_labels = torch.tensor(list(known_labels), device=self.device)
     pts = torch.cat(
       [self.prototypes[l.item()] for l in known_labels]
     )
+    known_labels = torch.tensor(list(known_labels), device=self.device)
 
     ### === Prepare data ===============================
     _, _, query_images, query_labels = batch
@@ -92,9 +92,9 @@ class RelationLearner:
     support_features = pts
     support_labels = known_labels
 
-    print(query_features.shape)
     print(support_features.shape)
-
+    print(query_features.shape)
+    
     ### === Concat features ============================
     support_features_ext = support_features.unsqueeze(0).repeat(n_known*args.query_num, 1, 1)  #[w*q, w*sh, 128]
     support_features_ext = torch.transpose(support_features_ext, 0, 1)                         #[w*sh, w*q, 128]
