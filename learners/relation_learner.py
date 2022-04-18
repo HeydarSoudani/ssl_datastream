9d23c5ac5eab866cb991a6ec04a3b86441fc1753
Heydar <heydar@Heydars-MacBook-Pro.local>
2022-04-18 12:03:06 +0430

    print labels shape

--- learners/relation_learner.py
+++ learners/relation_learner.py
@@ -423,15 +423,15 @@ class RelationLearner:
         else: # For score approach
           ## == Similarity test ================
           all_sim = cos_similarity(test_features, sup_features)
-          avg_sim = torch.tensor([
-            torch.mean(all_sim[:, i*rep_per_class:(i+1)*rep_per_class])
-            for i in known_labels
-          ])
-          prob, predicted_idx = torch.max(avg_sim, 0)
-          predicted_labels = known_labels[predicted_idx].item()
+          # avg_sim = torch.tensor([
+          #   torch.mean(all_sim[:, i*rep_per_class:(i+1)*rep_per_class])
+          #   for i in known_labels
+          # ])
+          # prob, predicted_idx = torch.max(avg_sim, 0)
+          # predicted_labels = known_labels[predicted_idx].item()
           
-          # _, ow_predict_labels = torch.max(all_sim, 1)
-          # predicted_labels = known_labels[torch.div(ow_predict_labels, args.n_examplers, rounding_mode='trunc')]
+          _, predicted_idx = torch.max(all_sim, 1)
+          predicted_labels = known_labels[torch.div(predicted_idx, args.n_examplers, rounding_mode='trunc')]
           
           ow_total += test_labels.size(0)
           ow_correct += (predicted_labels == test_labels).sum().item()
