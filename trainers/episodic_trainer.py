682e300ccf394a11e2fb108f9a9d335f4c14beed
Heydar <heydar@Heydars-MacBook-Pro.local>
2022-04-18 11:02:14 +0430

    print labels shape

--- trainers/episodic_trainer.py
+++ trainers/episodic_trainer.py
@@ -147,59 +147,59 @@ def separate_training(
   train_rel_loss = 0.
 
   ## = Fine-tune Feature extraction ===
-  # print('===== Feature extractor fine-tuning ... =====')
-  # trainloader = iter(train_dataloader)
-  # for miteration_item in range(args.meta_iteration):
-  #   batch = next(trainloader)
+  print('===== Feature extractor fine-tuning ... =====')
+  trainloader = iter(train_dataloader)
+  for miteration_item in range(args.meta_iteration):
+    batch = next(trainloader)
 
-  #   ext_loss = learner.feature_ext_train(
-  #     feature_ext,
-  #     batch,
-  #     feature_ext_optim,
-  #     args)
-  #   train_ext_loss += ext_loss
+    ext_loss = learner.feature_ext_train(
+      feature_ext,
+      batch,
+      feature_ext_optim,
+      args)
+    train_ext_loss += ext_loss
 
-  #   # = validation ===
-  #   if (miteration_item + 1) % args.log_interval == 0:
-  #     train_loss_total = train_ext_loss / args.log_interval
-  #     train_ext_loss = 0.
+    # = validation ===
+    if (miteration_item + 1) % args.log_interval == 0:
+      train_loss_total = train_ext_loss / args.log_interval
+      train_ext_loss = 0.
       
-  #     if args.rep_approach == 'prototype':
-  #       learner.calculate_prototypes(feature_ext, train_loader)
-  #     elif args.rep_approach == 'exampler':
-  #       learner.calculate_examplers(feature_ext, train_dataset, k=args.n_examplers)
+      if args.rep_approach == 'prototype':
+        learner.calculate_prototypes(feature_ext, train_loader)
+      elif args.rep_approach == 'exampler':
+        learner.calculate_examplers(feature_ext, train_dataset, k=args.n_examplers)
 
-  #     # evalute on val_dataset
-  #     val_loss, \
-  #     val_cw_acc, \
-  #     val_ow_acc \
-  #       = learner.evaluate(
-  #         feature_ext,
-  #         relation_net,
-  #         val_dataloader,
-  #         known_labels,
-  #         args)
+      # evalute on val_dataset
+      val_loss, \
+      val_cw_acc, \
+      val_ow_acc \
+        = learner.evaluate(
+          feature_ext,
+          relation_net,
+          val_dataloader,
+          known_labels,
+          args)
 
-  #     # print losses
-  #     # print('scheduler: %f' % (optim.param_groups[0]['lr']))
-  #     print('=== Time: %.2f, Step: %d, TrainLoss: %f, ValLoss: %f, Val-CwAcc: %.2f, Val-OwAcc: %.2f' % (
-  #       time.time()-global_time,
-  #       miteration_item+1,
-  #       train_loss_total,
-  #       val_loss,
-  #       val_cw_acc*100,
-  #       val_ow_acc*100
-  #     ))
-  #     global_time = time.time()
+      # print losses
+      # print('scheduler: %f' % (optim.param_groups[0]['lr']))
+      print('=== Time: %.2f, Step: %d, TrainLoss: %f, ValLoss: %f, Val-CwAcc: %.2f, Val-OwAcc: %.2f' % (
+        time.time()-global_time,
+        miteration_item+1,
+        train_loss_total,
+        val_loss,
+        val_cw_acc*100,
+        val_ow_acc*100
+      ))
+      global_time = time.time()
 
-  #     # save best feature_ext
-  #     if val_loss < min_loss:
-  #       feature_ext.save(os.path.join(args.save, "feature_ext_best.pt"))
-  #       min_loss = val_loss
-  #       print("= ...New best model saved")
+      # save best feature_ext
+      if val_loss < min_loss:
+        feature_ext.save(os.path.join(args.save, "feature_ext_best.pt"))
+        min_loss = val_loss
+        print("= ...New best model saved")
   
-  #   if args.scheduler:
-  #     feature_ext_scheduler.step()
+    if args.scheduler:
+      feature_ext_scheduler.step()
     
   print('===== Relation Network training ... =====')
   ## = Update Pts or Examplers =========
